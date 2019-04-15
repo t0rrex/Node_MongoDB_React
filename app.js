79725e1bb1ec24f769fdae2f35e1fbd0d5c18f34
Prometheus <glebus24@mail.ru>
2019-04-15 14:27:06 +0300

    Логгер, конфигурация, шаблонка для HTML

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
+var config = require('config');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var log = require('./libs/log')(module);
+
 
 var app = express();
-app.set('port', 3000);
+app.set('port', config.get('port'));
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-
-app.use(function (req, res, next) {
-    if (req.url == '/') {
-        res.end('Hello!');
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res, next) {
-    if (req.url == '/forbidden') {
-        next(new Error("Denied, sorry!"));
-    } else {
-        next();
-    }
-});
-
-app.use(function(req, res, next) {
-    res.send(404, 'Page Not Found!');
+http.createServer(app).listen(config.get('port'), function() {
+    log.info('Express server listening on port ' + app.get('port'));
 });
 
 // error handler

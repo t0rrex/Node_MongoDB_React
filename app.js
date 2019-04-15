4e429165190ad383902862413b9e863b1708513e
Prometheus <glebus24@mail.ru>
2019-04-15 08:16:06 +0300

    Express: основы и Middleware

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+
+var app = express();
+app.set('port', 3000);
+app.set('view engine', 'ejs');
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+app.use(function (req, res, next) {
+    if (req.url == '/') {
+        res.end('Hello!');
+    } else {
+        next();
+    }
+});
+
+app.use(function (req, res, next) {
+    if (req.url == '/forbidden') {
+        next(new Error("Denied, sorry!"));
+    } else {
+        next();
+    }
+});
+
+app.use(function(req, res, next) {
+    res.send(404, 'Page Not Found!');
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+/*/
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,12 +50,6 @@ var logger = require('morgan');
 var http = require('http');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var app = express();
-app.set('port', 3000);
-
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +61,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,4 +76,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app; */
